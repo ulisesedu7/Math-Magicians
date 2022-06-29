b0f1e1fbbbfc81d5bebbd7b194a1b2927235a152
Ulises Largaespada <ulisesedu7@users.noreply.github.com>
2022-06-29 14:34:54 -0600

    Start Calculator Function

---
+++ src/components/calculator.js
@@ -0,0 +1,15 @@
+class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <section className="calContainer">
+        <h2 className="calResult">0</h2>
+      </section>
+    );
+  }
+}
+
+export default Calculator;
