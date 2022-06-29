e031d7df3717a980147da1f7890fc7b5fa1953e5
Ulises Largaespada <ulisesedu7@users.noreply.github.com>
2022-06-29 17:36:28 -0600

    Fix Reviewer points

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+/* eslint-disable react/prefer-stateless-function */
+import { render } from '@testing-library/react';
+import React from 'react';
 import './App.css';
 import Calculator from './components/calculator';
 
-function App() {
-  return (
-    <Calculator />
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <Calculator />
+    );
+  }
 }
 
 export default App;
